e021be9ea7985e3c8e6bf8be87e393ac72268c28
Nathan Lucas <n54318879@gmail.com>
2023-01-19 18:11:55 -0300

    feat @types: criando tipagem do tailwindcss

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,8 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { Poppins_400Regular, Poppins_600SemiBold, Poppins_700Bold, Poppins_800ExtraBold } from '@expo-google-fonts/poppins';
 import { useFonts } from '@expo-google-fonts/poppins/useFonts';
 import { Loading } from './src/Components/Loading';
+import { Home } from './src/screens/Home';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -19,22 +19,9 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Open up App.tsx to start working on your app!</Text>
+    <>
+      <Home />
       <StatusBar style="auto" />
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#09090A',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    fontFamily: 'Poppins_400Regular',
-    color: '#fff'
-  }
-});
