392101ffcddcfae84169eb81bf8f76820985be07
Nathan Lucas <n54318879@gmail.com>
2023-01-20 16:28:35 -0300

    feat Header: criando modal para criação de habito

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,23 +1,48 @@
 import LogoImage from '../assets/logo.svg'
-import { Plus } from 'phosphor-react'
+import * as Dialog from '@radix-ui/react-dialog'
+import { Plus, X } from 'phosphor-react'
+import { motion } from 'framer-motion'
 export function Header () {
+
   return (
     <div className='w-full max-w-3xl mx-auto flex items-center justify-between'>
       <img src={LogoImage} alt="LogoHabits" />
 
-      <button
-        type='button'
+      <Dialog.Root>
 
-        className='
-        border border-violet-500
-        shadow-lg shadow-violet-500 
-        font-semibold rounded-lg 
-        px-6 py-4 flex items-center 
-        gap-3 hover:animate-pulse'
-      >
-        <Plus size={20} className='text-violet-500'/> 
-        Novo hábito
-      </button>
+        <Dialog.Trigger
+          type='button'
+          className='
+          border border-violet-500
+          shadow-lg shadow-violet-500 
+          font-semibold rounded-lg 
+          px-6 py-4 flex items-center 
+          gap-3 hover:animate-pulse'
+        >
+          <Plus size={20} className='text-violet-500'/> 
+          Novo hábito
+        </Dialog.Trigger>
+
+        <Dialog.Portal>
+          <Dialog.Overlay className='w-screen h-screen bg-black/50 fixed inset-0'/>
+
+          <Dialog.Content>
+            <motion.div 
+              className='absolute p-10 bg-zinc-900 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-2xl w-full max-w-md'
+              initial={{ height: '0'}}
+              animate={{ height: '50%'}}
+              >
+              <Dialog.Close className='absolute right-6 top-6 text-zinc-400 hover:text-zinc-200'>
+                <X size={24}/>
+              </Dialog.Close>
+
+              <Dialog.Title className='text-white font-extrabold'>
+                Criar novo hábito
+              </Dialog.Title>
+            </motion.div>
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root>
     </div>
   )
 }
