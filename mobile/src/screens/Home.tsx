ade61cb041d41432b3c7a2df4b112cafc171e9b3
Nathan Lucas <n54318879@gmail.com>
2023-01-19 22:07:18 -0300

    feat Components: criando components para o app

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,8 +1,64 @@
-import { View } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
+import { HabitDay, DaySize } from '../Components/Habitday'
+import { Header } from '../Components/Header'
+import { generateRangeDatesFromYearStart } from '../utils/generate-date-year-from-beginning';
+
+
+const weekDays = [
+  'Do',
+  'Se',
+  'Te',
+  'Qua',
+  'Qui',
+  'Sex',
+  'Sa'
+];
+
+const dateFromYearStart = generateRangeDatesFromYearStart();
+const minimunSummaryDatesSizes = 18 * 5;
+const amountOfDaysToFill = minimunSummaryDatesSizes - dateFromYearStart.length
 
 export function Home() {
   return (
-    <View>
+    <View className='flex-1 bg-background px-8 py-16'>
+      <Header />
+
+      <ScrollView
+      showsHorizontalScrollIndicator={false}
+      contentContainerStyle={{ paddingBottom: 100 }}
+      >
+        <View className='flex-row pt-6 pb-4'>
+          {weekDays.map((weekDays, i) => (
+            <Text key={`${weekDays}-${i}`}
+            className='text-zinc-500 font-bold text-center text-xl mx-1'
+            style={{ width: DaySize}}
+            >
+              {weekDays}
+            </Text>
+          ))}
+        </View>
+
+        <View className='flex-row flex-wrap'>
+          {dateFromYearStart.map(date => (
+            <HabitDay
+              key={date.toISOString()} 
+            />
+          ))}
+
+        {
+          amountOfDaysToFill > 0 && Array
+          .from({ length: amountOfDaysToFill })
+          .map((_, index) => (
+            <View 
+              key={index}
+              className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
+              style={{ width: DaySize, height: DaySize }}
+            />
+          ))
+        }
+        </View>
+      </ScrollView>
+
       
     </View>
   )
